286ceb2611ea72476a95b7cbe1651a396f3b8ca9
MihirSharma2612 <mihirsharmamihirsharma3@gmail.com>
2024-03-09 20:37:11 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// console.log("This is my testing");
+
+function makeBubble(){
+
+    var clutter = "";
+
+    for(var i=1; i<=168; i++ ){
+        var rn = Math.floor(Math.random()*10)
+       clutter += `<div class="bubble">${rn}</div>`;
+    }
+    
+    document.querySelector("#pbtm").innerHTML = clutter;
+}
+makeBubble();
+
+
+var timer = 60;
+   function runTimer() {
+      var timerint= setInterval(function(){
+        if(timer>0){
+            timer--;
+            document.querySelector("#timerval").textContent = timer;
+        } else{
+            clearInterval(timerint);
+        }       
+      },1000);
+   }
+runTimer();
+
+
+function getNewHit(){
+   hitrn = Math.floor(Math.random() * 10);
+   document.querySelector("#hitval").textContent = hitrn;
+}
+getNewHit();
+
+
+var score = 0;
+function increaseScore(){
+     score += 10;
+     document.querySelector("#scoreval").textContent = hitrn;
+}
+
+var hitrn = 0;
+document.querySelector("#pbtm")
+.addEventListener("click",function(dets){
+    var clickednum = Number(dets.target.textContent);
+    if(clickednum === hitrn){
+      increaseScore();        
+      makeBubble();
+      getNewHit();
+    }
+
+});
